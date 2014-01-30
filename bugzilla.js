cb9fb673c332f6b39e80fcb0997edd1c8d40f597
blackbird- <milovidov.dd@hotmail.com>
2014-01-30 20:13:26 +0400

    got some review already

--- bugzilla.js
+++ bugzilla.js
@@ -3,105 +3,89 @@ comment = document.getElementById('comment');
 
 severity = document.getElementById('bug_severity');
 priority = document.getElementById('priority');
-	
+ 
 commit_top = document.getElementById('commit_top');
-commit = document.getElementById('commit');	
+commit = document.getElementById('commit'); 
 
-function focusonload() {
+function focusonload() { 
+// may be it should be
+// in next function?
 
-	function cursorfocus(s) {
-	x = window.scrollX;
-    y = window.scrollY;
-    s.focus();
-    window.scrollTo(x, y);
-	}
+    function cursorfocus(s) {
+        x = window.scrollX;
+        y = window.scrollY;
+        s.focus();
+        window.scrollTo(x, y);
+    }
 
-	if (comment !== null) {
-		cursorfocus(comment)
-	} else {quicksearch.focus();
-	}
+    if (comment !== null) {
+        cursorfocus(comment)
+    } else {
+        quicksearch.focus();
+    }
 }
 
 function navigation(keypressed) { 
 
     keypressed = keypressed || window.event;
 
-	function selectelement(w, select) {
-    w.value = select;
-	}
-	
+     function selectelement(w, select) {
+        w.value = select;
+     }
+ 
     keyCode = keypressed.keyCode || keypressed.which,
-        kn = {
-            enter: 13, // ctrl + enter - оставить комментарий
-            save: 83, // alt + s - сохранить изменения бага
-            down: 40, // crtl + down - в поле комментария
-            up: 38, // ctrl + up - в поле поиска
-            p1: 49, // alt + 1 - P1 - приоритет
-            p2: 50, // alt + 2 - P2 - приоритет 
-            p3: 51, // alt + 3 - P3 - приоритет
-            p4: 52, // alt + 4 - P4 - приоритет
-            p5: 53, // alt + 5 - P5 - приоритет
-        };
-				
+    kn = {
+        enter: 13, 
+        save: 83, 
+        down: 40, 
+        up: 38, 
+        p1: 49, 
+        p2: 50, 
+        p3: 51, 
+        p4: 52, 
+        p5: 53, 
+    };
+    
     if (keypressed.altKey) {
-        switch (keyCode) {
-            case kn.p1:
-                if (priority!==null) {
-				selectelement(priority, "P1");
-                severity.focus();
-				}
-                break;
-            case kn.p2:
-                if (priority!==null) {
-				selectelement(priority, "P2");
-                severity.focus();
-				}
-                break;
-            case kn.p3:
-                if (priority!==null) {
-				selectelement(priority, "P3");
-                severity.focus();
-				}
-                break;
-            case kn.p4:
-                if (priority!==null) {
-				selectelement(priority, "P4");
-                severity.focus();
-				}
-                break;
-            case kn.p5:
-                if (priority!==null) {
-				selectelement(priority, "P5");
-                severity.focus();
-				}
-                break;
-			case kn.save:
-                if (commit_top!==null) {
-				commit_top.click();
-				}
-                break;
+        if (keyCode == nk.save && commit_top!==null){
+            commit_top.click();
         }
 
+        var key_str;
+        if (priority!==null){
+            switch (keyCode) {
+                case kn.p1: key_str = "P1"; break;
+                case kn.p2: key_str = "P2"; break;
+                case kn.p3: key_str = "P3"; break;
+                case kn.p4: key_str = "P4"; break;
+                case kn.p5: key_str = "P5"; break;
+            }
+        }
+        selectelement(priority, key_str);
+        severity.focus();
+		
     } else if (keypressed.ctrlKey) {
         switch (keyCode) {
             case kn.enter:
                 if (commit!==null&&comment.value!=="") {
-				commit.click();
-				}
+                    commit.click();
+                }
                 break;
-			case kn.up:
+            case kn.up:
                 if (quicksearch!==null) {
-				quicksearch.focus();
-				}
+                    quicksearch.focus();
+                }
                 break;
-			case kn.down:
+            case kn.down:
                 if (comment!==null) {
-				comment.focus();
-				}
+                    comment.focus();
+                }
                 break;
-		}
+        }
     }
 }
 
 focusonload();
 document.onkeydown = navigation;
+// how to call this functions when page
+// would be loaded at all (with window.onload)
